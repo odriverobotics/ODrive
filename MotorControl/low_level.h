4ca24d89b10a7dda89877a0ca7a8024a0a8898e0
Oskar Weigl <oskar.weigl@rapyuta-robotics.com>
2017-02-05 04:25:10 +0100

    add comment

--- MotorControl/low_level.h
+++ MotorControl/low_level.h
@@ -72,6 +72,7 @@ extern const int num_motors;
 /* Exported macro ------------------------------------------------------------*/
 /* Exported functions --------------------------------------------------------*/
 
+//Note: to control without feed forward, set feed forward terms to 0.0f.
 void set_pos_setpoint(Motor_t* motor, float pos_setpoint, float vel_feed_forward, float current_feed_forward);
 void set_vel_setpoint(Motor_t* motor, float vel_setpoint, float current_feed_forward);
 void set_current_setpoint(Motor_t* motor, float current_setpoint);
