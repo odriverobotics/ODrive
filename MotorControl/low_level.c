2a727c6f2d62a61f813a9665dcc0c799f558e05d
Oskar Weigl <oskar.weigl@gmail.com>
2017-08-24 22:43:06 -0700

    always float motor after calibration

--- MotorControl/low_level.c
+++ MotorControl/low_level.c
@@ -1257,9 +1257,7 @@ void motor_thread(void const * argument) {
         if (motor->do_calibration) {
             __HAL_TIM_MOE_ENABLE(motor->motor_timer);// enable pwm outputs
             motor_calibration(motor);
-            if(!motor->calibration_ok){
-                __HAL_TIM_MOE_DISABLE_UNCONDITIONALLY(motor->motor_timer);// disables pwm outputs
-            }
+            __HAL_TIM_MOE_DISABLE_UNCONDITIONALLY(motor->motor_timer);// disables pwm outputs
             motor->do_calibration = false;
         }
         
