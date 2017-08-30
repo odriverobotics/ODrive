1952280c232f48a12481eb47537acf96163ff488
Unknown <pfguenette@gmail.com>
2017-08-30 18:18:48 -0400

    Convert Sensorless to C++

--- MotorControl/sensorless.h
+++ MotorControl/sensorless.h
@@ -1,18 +1,24 @@
 #pragma once
 
-typedef struct
-{
-    float phase;
-    float pll_pos;
-    float pll_vel;
-    float pll_kp;
-    float pll_ki;
-    float observer_gain;           // [rad/s]
-    float flux_state[2];           // [Vs]
-    float V_alpha_beta_memory[2];  // [V]
-    float pm_flux_linkage;         // [V / (rad/s)]
-    bool estimator_good;
-    float spin_up_current;       // [A]
-    float spin_up_acceleration;  // [rad/s^2]
-    float spin_up_target_vel;    // [rad/s]
-} ODrive_Sensorless_t;
+class Sensorless{
+public:
+    Sensorless();
+
+    // TODO:  Get/set functions as needed
+    float phase = 0.0f;
+    float pll_pos = 0.0f;
+    float pll_vel = 0.0f;
+    float pll_kp = 0.0f;
+    float pll_ki = 0.0f;
+    float observer_gain = 1000.0f;           // [rad/s]
+    float flux_state[] = {0.0f, 0.0f};           // [Vs]
+    float V_alpha_beta_memory[] = {0.0f, 0.0f};  // [V]
+    float pm_flux_linkage = 1.538e-3f;         // [V / (rad/s)] { 5.51328895422 / (<pole pairs> * <rpm/v>) }
+    bool estimator_good = false;
+    float spin_up_current = 10.0f;       // [A]
+    float spin_up_acceleration = 400.0f;  // [rad/s^2]
+    float spin_up_target_vel = 400.0f;    // [rad/s]
+
+    // TODO:  Functions which operate on Sensorless objects should be here
+}
+
