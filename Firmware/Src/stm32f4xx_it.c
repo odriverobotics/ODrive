1d48659367776d7deade64b69c1b926388036249
Oskar Weigl <oskar.weigl@gmail.com>
2017-11-17 04:48:37 -0800

    interrupt vectors no longer different between HW versions

--- Firmware/Src/stm32f4xx_it.c
+++ Firmware/Src/stm32f4xx_it.c
@@ -43,10 +43,6 @@
 typedef void (*ADC_handler_t)(ADC_HandleTypeDef* hadc, bool injected);
 void ADC_IRQ_Dispatch(ADC_HandleTypeDef* hadc, ADC_handler_t callback);
 
-#if HW_VERSION_MAJOR == 3 && HW_VERSION_MINOR == 1 \
-||  HW_VERSION_MAJOR == 3 && HW_VERSION_MINOR == 2
-#include "prev_board_ver/stm32f4xx_it_V3_2.c"
-#else
 /* USER CODE END 0 */
 
 /* External variables --------------------------------------------------------*/
@@ -281,7 +277,6 @@ void OTG_FS_IRQHandler(void)
 }
 
 /* USER CODE BEGIN 1 */
-#endif
 
 void ADC_IRQ_Dispatch(ADC_HandleTypeDef* hadc, ADC_handler_t callback) {
 
