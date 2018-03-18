fcdbde26409af2d9c97d3ff53d18b1b88af28e0a
Samuel Sadok <samuel.sadok@bluewin.ch>
2018-03-17 22:11:58 -0700

    build for multiple configurations

---
+++ tools/build.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+# Builds the firmware with the configuration specified by
+# environment variables named CONFIG_...
+# If DEPLOY is set, the deliverables are copied to Firmware/deploy/*
+# with the suffix $DEPLOY
+set -euo pipefail
+
+THIS_DIR="$(dirname "$0")"
+cd "$THIS_DIR/../Firmware"
+
+# Write all environment variables that start with "CONFIG_" to tup.config
+mkdir -p build
+rm build/*
+env | grep ^CONFIG > tup.config
+tup generate ./tup_build.sh
+bash -xe ./tup_build.sh
+
+# Deploy
+if ! [ -z ${DEPLOY+x} ]; then
+    mkdir -p deploy
+    cp build/ODriveFirmware.elf deploy/ODriveFirmware-"$DEPLOY".elf
+    cp build/ODriveFirmware.hex deploy/ODriveFirmware-"$DEPLOY".hex
+fi
