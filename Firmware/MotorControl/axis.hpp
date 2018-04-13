bd292b4775d5964e82a49287f793943f0fa512b0
Samuel Sadok <samuel.sadok@bluewin.ch>
2018-04-13 16:17:05 -0700

    fix comment

--- Firmware/MotorControl/axis.hpp
+++ Firmware/MotorControl/axis.hpp
@@ -13,7 +13,7 @@ enum AxisState_t {
     AXIS_STATE_STARTUP_SEQUENCE = 2, //<! the actual sequence is defined by the config.startup_... flags
     AXIS_STATE_FULL_CALIBRATION_SEQUENCE = 3,   //<! run all calibration procedures, then idle
     AXIS_STATE_MOTOR_CALIBRATION = 4,   //<! run motor calibration
-    AXIS_STATE_SENSORLESS_CONTROL = 5,  //<! run sensorless calibration
+    AXIS_STATE_SENSORLESS_CONTROL = 5,  //<! run sensorless control
     AXIS_STATE_ENCODER_INDEX_SEARCH = 6, //<! run encoder index search
     AXIS_STATE_ENCODER_OFFSET_CALIBRATION = 7, //<! run encoder offset calibration
     AXIS_STATE_CLOSED_LOOP_CONTROL = 8  //<! run closed loop control
