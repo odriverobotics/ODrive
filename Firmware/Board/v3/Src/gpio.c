8df5e217b0d880d34ce51739dbb062ac62f3f7df
Unknown <pfguenette@gmail.com>
2018-08-29 22:39:32 -0400

    Add missing pin #'s in get_gpio_by_x functions

--- Firmware/Board/v3/Src/gpio.c
+++ Firmware/Board/v3/Src/gpio.c
@@ -286,6 +286,15 @@ GPIO_TypeDef* get_gpio_port_by_pin(uint16_t GPIO_pin){
 #ifdef GPIO_5_GPIO_Port
     case 5: return GPIO_5_GPIO_Port; break;
 #endif
+#ifdef GPIO_6_GPIO_Port
+    case 6: return GPIO_6_GPIO_Port; break;
+#endif
+#ifdef GPIO_7_GPIO_Port
+    case 7: return GPIO_7_GPIO_Port; break;
+#endif
+#ifdef GPIO_8_GPIO_Port
+    case 8: return GPIO_8_GPIO_Port; break;
+#endif
     default: return GPIO_1_GPIO_Port;
   }
 }
@@ -298,6 +307,15 @@ uint16_t get_gpio_pin_by_pin(uint16_t GPIO_pin){
     case 4: return GPIO_4_Pin; break;
 #ifdef GPIO_5_Pin
     case 5: return GPIO_5_Pin; break;
+#endif
+#ifdef GPIO_6_Pin
+    case 6: return GPIO_6_Pin; break;
+#endif
+#ifdef GPIO_7_Pin
+    case 7: return GPIO_7_Pin; break;
+#endif
+#ifdef GPIO_8_Pin
+    case 8: return GPIO_8_Pin; break;
 #endif
     default: return GPIO_1_Pin;
   }
