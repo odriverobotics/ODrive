9964697b2f4b631751c6b3a3f6302f69802be783
Akex2 <schointss@gmail.com>
2018-09-02 10:17:00 +0200

    Add files via upload

---
+++ axis.cpp
@@ -0,0 +1,361 @@
+
+#include <stdlib.h>
+#include <functional>
+#include "gpio.h"
+
+#include "utils.h"
+#include "odrive_main.h"
+
+Axis::Axis(const AxisHardwareConfig_t& hw_config,
+           AxisConfig_t& config,
+           Encoder& encoder,
+           SensorlessEstimator& sensorless_estimator,
+           Controller& controller,
+           Motor& motor)
+    : hw_config_(hw_config),
+      config_(config),
+      encoder_(encoder),
+      sensorless_estimator_(sensorless_estimator),
+      controller_(controller),
+      motor_(motor)
+{
+    encoder_.axis_ = this;
+    sensorless_estimator_.axis_ = this;
+    controller_.axis_ = this;
+    motor_.axis_ = this;
+}
+
+static void step_cb_wrapper(void* ctx) {
+    reinterpret_cast<Axis*>(ctx)->step_cb();
+}
+
+// @brief Sets up all components of the axis,
+// such as gate driver and encoder hardware.
+void Axis::setup() {
+    encoder_.setup();
+    motor_.setup();
+}
+
+static void run_state_machine_loop_wrapper(void* ctx) {
+    reinterpret_cast<Axis*>(ctx)->run_state_machine_loop();
+    reinterpret_cast<Axis*>(ctx)->thread_id_valid_ = false;
+}
+
+// @brief Starts run_state_machine_loop in a new thread
+void Axis::start_thread() {
+    osThreadDef(thread_def, run_state_machine_loop_wrapper, hw_config_.thread_priority, 0, 4*512);
+    thread_id_ = osThreadCreate(osThread(thread_def), this);
+    thread_id_valid_ = true;
+}
+
+// @brief Unblocks the control loop thread.
+// This is called from the current sense interrupt handler.
+void Axis::signal_current_meas() {
+    if (thread_id_valid_)
+        osSignalSet(thread_id_, M_SIGNAL_PH_CURRENT_MEAS);
+}
+
+// @brief Blocks until a current measurement is completed
+// @returns True on success, false otherwise
+bool Axis::wait_for_current_meas() {
+    return osSignalWait(M_SIGNAL_PH_CURRENT_MEAS, PH_CURRENT_MEAS_TIMEOUT).status == osEventSignal;
+}
+
+// step/direction interface
+void Axis::step_cb() {
+    if (enable_step_dir_) {
+        GPIO_PinState dir_pin = HAL_GPIO_ReadPin(hw_config_.dir_port, hw_config_.dir_pin);
+        float dir = (dir_pin == GPIO_PIN_SET) ? 1.0f : -1.0f;
+        controller_.pos_setpoint_ += dir * config_.counts_per_step;
+    }
+};
+
+// @brief Enables or disables step/dir input
+void Axis::set_step_dir_enabled(bool enable) {
+    if (enable) {
+        // Set up the direction GPIO as input
+        GPIO_InitTypeDef GPIO_InitStruct;
+        GPIO_InitStruct.Pin = hw_config_.dir_pin;
+        GPIO_InitStruct.Mode = GPIO_MODE_INPUT;
+        GPIO_InitStruct.Pull = GPIO_NOPULL;
+        HAL_GPIO_Init(hw_config_.dir_port, &GPIO_InitStruct);
+
+        // Subscribe to rising edges of the step GPIO
+        GPIO_subscribe(hw_config_.step_port, hw_config_.step_pin, GPIO_PULLDOWN,
+                step_cb_wrapper, this);
+
+        enable_step_dir_ = true;
+    } else {
+        enable_step_dir_ = false;
+
+        // Unsubscribe from step GPIO
+        GPIO_unsubscribe(hw_config_.step_port, hw_config_.step_pin);
+    }
+}
+
+void Axis::set_motor_enable_pin_enabled(bool enable) {
+    if (enable) {
+        // Set up the direction GPIO as input
+        GPIO_InitTypeDef GPIO_InitStruct;
+        //GPIO_InitStruct.Pin = hw_config_.motor_enable_pin;
+        GPIO_InitStruct.Pin = GPIO_5_Pin;
+        GPIO_InitStruct.Mode = GPIO_MODE_INPUT;
+        GPIO_InitStruct.Pull = GPIO_NOPULL;
+        //HAL_GPIO_Init(hw_config_.motor_enable_port, &GPIO_InitStruct);
+        HAL_GPIO_Init(GPIO_5_GPIO_Port, &GPIO_InitStruct);
+
+        enable_motor_pin = true;
+    } else {
+        enable_motor_pin = false;
+
+        // Unsubscribe from step GPIO
+        GPIO_unsubscribe(GPIO_5_GPIO_Port, GPIO_5_Pin);
+    }
+}
+
+bool Axis::check_for_errors() {
+    // Maybe we should update this to only trigger on new errors?
+    // The danger with that is we could fail to bail on uncleared errors that still prevent
+    // correct opreation.
+
+    // For now: we treat ERROR_INVALID_STATE in idle loop special, or we could never stay
+    // in idle after this kind of error.
+    if (current_state_ == AXIS_STATE_IDLE)
+        return (error_ & ~ERROR_INVALID_STATE) == ERROR_NONE;
+    else
+        return error_ == ERROR_NONE;
+}
+
+bool Axis::check_next_state() {
+    if (enable_motor_pin == true) {
+        bool pinstate = (HAL_GPIO_ReadPin(GPIO_5_GPIO_Port, GPIO_5_Pin));
+        if (pinstate == true) {
+            if (current_state_ == AXIS_STATE_CLOSED_LOOP_CONTROL) {
+                requested_state_ = AXIS_STATE_IDLE;
+            }
+        }
+        if (pinstate == false ) {
+            if (current_state_ == AXIS_STATE_IDLE) {
+                encoder_.set_linear_count(0);
+                requested_state_ = AXIS_STATE_CLOSED_LOOP_CONTROL;
+            }       
+        }
+    }
+    return check_for_errors();            
+}
+// @brief Do axis level checks and call subcomponent do_checks
+// Returns true if everything is ok.
+bool Axis::do_checks() {
+    if (!brake_resistor_armed)
+        error_ |= ERROR_BRAKE_RESISTOR_DISARMED;
+    if ((current_state_ != AXIS_STATE_IDLE) && (motor_.armed_state_ == Motor::ARMED_STATE_DISARMED))
+        // motor got disarmed in something other than the idle loop
+        error_ |= ERROR_MOTOR_DISARMED;
+    if (!(vbus_voltage >= board_config.dc_bus_undervoltage_trip_level))
+        error_ |= ERROR_DC_BUS_UNDER_VOLTAGE;
+    if (!(vbus_voltage <= board_config.dc_bus_overvoltage_trip_level))
+        error_ |= ERROR_DC_BUS_OVER_VOLTAGE;
+
+    // Sub-components should use set_error which will propegate to this error_
+    motor_.do_checks();
+    encoder_.do_checks();
+    // sensorless_estimator_.do_checks();
+    // controller_.do_checks();
+
+    return check_for_errors();
+}
+
+// @brief Update all esitmators
+bool Axis::do_updates() {
+    // Sub-components should use set_error which will propegate to this error_
+    encoder_.update();
+    sensorless_estimator_.update();
+    return check_for_errors();
+}
+
+float Axis::get_temp() {
+    float adc = adc_measurements_[hw_config_.thermistor_adc_ch];
+    float normalized_voltage = adc / adc_full_scale;
+    return horner_fma(normalized_voltage, thermistor_poly_coeffs, thermistor_num_coeffs);
+}
+
+bool Axis::run_sensorless_spin_up() {
+    // Early Spin-up: spiral up current
+    float x = 0.0f;
+    run_control_loop([&](){
+        float phase = wrap_pm_pi(config_.ramp_up_distance * x);
+        float I_mag = config_.spin_up_current * x;
+        x += current_meas_period / config_.ramp_up_time;
+        if (!motor_.update(I_mag, phase))
+            return error_ |= ERROR_MOTOR_FAILED, false;
+        return x < 1.0f;
+    });
+    if (error_ != ERROR_NONE)
+        return false;
+    
+    // Late Spin-up: accelerate
+    float vel = config_.ramp_up_distance / config_.ramp_up_time;
+    float phase = wrap_pm_pi(config_.ramp_up_distance);
+    run_control_loop([&](){
+        vel += config_.spin_up_acceleration * current_meas_period;
+        phase = wrap_pm_pi(phase + vel * current_meas_period);
+        float I_mag = config_.spin_up_current;
+        if (!motor_.update(I_mag, phase))
+            return error_ |= ERROR_MOTOR_FAILED, false;
+        return vel < config_.spin_up_target_vel;
+    });
+
+    // call to controller.reset() that happend when arming means that vel_setpoint
+    // is zeroed. So we make the setpoint the spinup target for smooth transition.
+    controller_.vel_setpoint_ = config_.spin_up_target_vel;
+
+    return check_for_errors();
+}
+
+// Note run_sensorless_control_loop and run_closed_loop_control_loop are very similar and differ only in where we get the estimate from.
+bool Axis::run_sensorless_control_loop() {
+    set_step_dir_enabled(config_.enable_step_dir);
+    set_motor_enable_pin_enabled(config_.enable_motor_pin);
+    run_control_loop([this](){
+        if (controller_.config_.control_mode >= CTRL_MODE_POSITION_CONTROL)
+            return error_ |= ERROR_POS_CTRL_DURING_SENSORLESS, false;
+
+        // Note that all estimators are updated in the loop prefix in run_control_loop
+        float current_setpoint;
+        if (!controller_.update(sensorless_estimator_.pll_pos_, sensorless_estimator_.vel_estimate_, &current_setpoint))
+            return error_ |= ERROR_CONTROLLER_FAILED, false;
+        if (!motor_.update(current_setpoint, sensorless_estimator_.phase_))
+            return false; // set_error should update axis.error_
+        return true;
+    });
+    set_step_dir_enabled(false);
+    return check_for_errors();
+}
+
+bool Axis::run_closed_loop_control_loop() {
+    set_step_dir_enabled(config_.enable_step_dir);
+    set_motor_enable_pin_enabled(config_.enable_motor_pin);
+    run_control_loop([this](){
+        // Note that all estimators are updated in the loop prefix in run_control_loop
+        float current_setpoint;
+        if (!controller_.update(encoder_.pos_estimate_, encoder_.vel_estimate_, &current_setpoint))
+            return error_ |= ERROR_CONTROLLER_FAILED, false; //TODO: Make controller.set_error
+        if (!motor_.update(current_setpoint, encoder_.phase_))
+            return false; // set_error should update axis.error_
+        return true;
+    });
+    set_step_dir_enabled(false);
+    return check_for_errors();
+}
+
+bool Axis::run_idle_loop() {
+    // run_control_loop ignores missed modulation timing updates
+    // if and only if we're in AXIS_STATE_IDLE
+    safety_critical_disarm_motor_pwm(motor_);
+    run_control_loop([this](){
+        return true;
+    });
+    return check_for_errors();
+}
+
+// Infinite loop that does calibration and enters main control loop as appropriate
+void Axis::run_state_machine_loop() {
+
+    // Allocate the map for anti-cogging algorithm and initialize all values to 0.0f
+    // TODO: Move this somewhere else
+    // TODO: respect changes of CPR
+    int encoder_cpr = encoder_.config_.cpr;
+    controller_.anticogging_.cogging_map = (float*)malloc(encoder_cpr * sizeof(float));
+    if (controller_.anticogging_.cogging_map != NULL) {
+        for (int i = 0; i < encoder_cpr; i++) {
+            controller_.anticogging_.cogging_map[i] = 0.0f;
+        }
+    }
+
+    // arm!
+    motor_.arm();
+    
+    for (;;) {
+        // Load the task chain if a specific request is pending
+        if (requested_state_ != AXIS_STATE_UNDEFINED) {
+            size_t pos = 0;
+            if (requested_state_ == AXIS_STATE_STARTUP_SEQUENCE) {
+                if (config_.startup_motor_calibration)
+                    task_chain_[pos++] = AXIS_STATE_MOTOR_CALIBRATION;
+                if (config_.startup_encoder_index_search && encoder_.config_.use_index)
+                    task_chain_[pos++] = AXIS_STATE_ENCODER_INDEX_SEARCH;
+                if (config_.startup_encoder_offset_calibration)
+                    task_chain_[pos++] = AXIS_STATE_ENCODER_OFFSET_CALIBRATION;
+                if (config_.startup_closed_loop_control)
+                    task_chain_[pos++] = AXIS_STATE_CLOSED_LOOP_CONTROL;
+                else if (config_.startup_sensorless_control)
+                    task_chain_[pos++] = AXIS_STATE_SENSORLESS_CONTROL;
+                task_chain_[pos++] = AXIS_STATE_IDLE;
+            } else if (requested_state_ == AXIS_STATE_FULL_CALIBRATION_SEQUENCE) {
+                task_chain_[pos++] = AXIS_STATE_MOTOR_CALIBRATION;
+                if (encoder_.config_.use_index)
+                    task_chain_[pos++] = AXIS_STATE_ENCODER_INDEX_SEARCH;
+                task_chain_[pos++] = AXIS_STATE_ENCODER_OFFSET_CALIBRATION;
+                task_chain_[pos++] = AXIS_STATE_IDLE;
+            } else if (requested_state_ != AXIS_STATE_UNDEFINED) {
+                task_chain_[pos++] = requested_state_;
+                task_chain_[pos++] = AXIS_STATE_IDLE;
+            }
+            task_chain_[pos++] = AXIS_STATE_UNDEFINED; // TODO: bounds checking
+            requested_state_ = AXIS_STATE_UNDEFINED;
+            // Auto-clear any invalid state error
+            error_ &= ~ERROR_INVALID_STATE;
+        }
+
+        // Note that current_state is a reference to task_chain_[0]
+
+        // Validate the state before running it
+        if (current_state_ > AXIS_STATE_MOTOR_CALIBRATION && !motor_.is_calibrated_)
+            current_state_ = AXIS_STATE_UNDEFINED;
+        if (current_state_ > AXIS_STATE_ENCODER_OFFSET_CALIBRATION && !encoder_.is_ready_)
+            current_state_ = AXIS_STATE_UNDEFINED;
+
+        // Run the specified state
+        // Handlers should exit if requested_state != AXIS_STATE_UNDEFINED
+        bool status;
+        switch (current_state_) {
+            case AXIS_STATE_MOTOR_CALIBRATION:
+                status = motor_.run_calibration();
+                break;
+
+            case AXIS_STATE_ENCODER_INDEX_SEARCH:
+                status = encoder_.run_index_search();
+                break;
+
+            case AXIS_STATE_ENCODER_OFFSET_CALIBRATION:
+                status = encoder_.run_offset_calibration();
+                break;
+
+            case AXIS_STATE_SENSORLESS_CONTROL:
+                status = run_sensorless_spin_up(); // TODO: restart if desired
+                if (status)
+                    status = run_sensorless_control_loop();
+                break;
+
+            case AXIS_STATE_CLOSED_LOOP_CONTROL:
+                status = run_closed_loop_control_loop();
+                break;
+
+            case AXIS_STATE_IDLE:
+                run_idle_loop();
+                status = motor_.arm(); // done with idling - try to arm the motor
+                break;
+
+            default:
+                error_ |= ERROR_INVALID_STATE;
+                status = false; // this will set the state to idle
+                break;
+        }
+
+        // If the state failed, go to idle, else advance task chain
+        if (!status)
+            current_state_ = AXIS_STATE_IDLE;
+        else
+            memcpy(task_chain_, task_chain_ + 1, sizeof(task_chain_) - sizeof(task_chain_[0]));
+    }
+}
---
+++ board_config_v3.h
@@ -0,0 +1,177 @@
+/*
+* @brief Contains board specific configuration for ODrive v3.x
+*/
+
+#ifndef __BOARD_CONFIG_H
+#define __BOARD_CONFIG_H
+
+// STM specific includes
+#include <gpio.h>
+#include <spi.h>
+#include <tim.h>
+#include <main.h>
+
+#if HW_VERSION_MAJOR == 3
+#if HW_VERSION_MINOR <= 3
+#define SHUNT_RESISTANCE (675e-6f)
+#else
+#define SHUNT_RESISTANCE (500e-6f)
+#endif
+#endif
+
+
+typedef struct {
+    GPIO_TypeDef* step_port;
+    uint16_t step_pin;
+    GPIO_TypeDef* dir_port;
+    uint16_t dir_pin;
+    //GPIO_TypeDef* motor_enable_port;
+    //uint16_t motor_enable_pin;
+    size_t thermistor_adc_ch;
+    osPriority thread_priority;
+} AxisHardwareConfig_t;
+
+typedef struct {
+    TIM_HandleTypeDef* timer;
+    GPIO_TypeDef* index_port;
+    uint16_t index_pin;
+    GPIO_TypeDef* hallA_port;
+    uint16_t hallA_pin;
+    GPIO_TypeDef* hallB_port;
+    uint16_t hallB_pin;
+    GPIO_TypeDef* hallC_port;
+    uint16_t hallC_pin;
+} EncoderHardwareConfig_t;
+typedef struct {
+    TIM_HandleTypeDef* timer;
+    uint16_t control_deadline;
+    float shunt_conductance;
+} MotorHardwareConfig_t;
+typedef struct {
+    SPI_HandleTypeDef* spi;
+    GPIO_TypeDef* enable_port;
+    uint16_t enable_pin;
+    GPIO_TypeDef* nCS_port;
+    uint16_t nCS_pin;
+    GPIO_TypeDef* nFAULT_port;
+    uint16_t nFAULT_pin;
+} GateDriverHardwareConfig_t;
+typedef struct {
+    AxisHardwareConfig_t axis_config;
+    EncoderHardwareConfig_t encoder_config;
+    MotorHardwareConfig_t motor_config;
+    GateDriverHardwareConfig_t gate_driver_config;
+} BoardHardwareConfig_t;
+
+extern const BoardHardwareConfig_t hw_configs[2];
+extern const float thermistor_poly_coeffs[];
+extern const size_t thermistor_num_coeffs;
+
+//TODO stick this in a C file
+#ifdef __MAIN_CPP__
+const float thermistor_poly_coeffs[] =
+    {363.0172658f, -459.19773008f, 308.29273921f, -28.12731452f};
+const size_t thermistor_num_coeffs = sizeof(thermistor_poly_coeffs)/sizeof(thermistor_poly_coeffs[1]);
+
+const BoardHardwareConfig_t hw_configs[2] = { {
+    //M0
+    .axis_config = {
+        .step_port = GPIO_1_GPIO_Port,
+        .step_pin = GPIO_1_Pin,
+        .dir_port = GPIO_2_GPIO_Port,
+        .dir_pin = GPIO_2_Pin,
+        //.motor_enable_pin = GPIO_5_Pin,
+        //.motor_enable_port = GPIO_5_GPIO_Port,
+        .thermistor_adc_ch = 15,
+        .thread_priority = (osPriority)(osPriorityHigh + (osPriority)1),
+    },
+    .encoder_config = {
+        .timer = &htim3,
+        .index_port = M0_ENC_Z_GPIO_Port,
+        .index_pin = M0_ENC_Z_Pin,
+        .hallA_port = M0_ENC_A_GPIO_Port,
+        .hallA_pin = M0_ENC_A_Pin,
+        .hallB_port = M0_ENC_B_GPIO_Port,
+        .hallB_pin = M0_ENC_B_Pin,
+        .hallC_port = M0_ENC_Z_GPIO_Port,
+        .hallC_pin = M0_ENC_Z_Pin,
+    },
+    .motor_config = {
+        .timer = &htim1,
+        .control_deadline = TIM_1_8_PERIOD_CLOCKS,
+        .shunt_conductance = 1.0f / SHUNT_RESISTANCE,  //[S]
+    },
+    .gate_driver_config = {
+        .spi = &hspi3,
+        // Note: this board has the EN_Gate pin shared!
+        .enable_port = EN_GATE_GPIO_Port,
+        .enable_pin = EN_GATE_Pin,
+        .nCS_port = M0_nCS_GPIO_Port,
+        .nCS_pin = M0_nCS_Pin,
+        .nFAULT_port = nFAULT_GPIO_Port, // the nFAULT pin is shared between both motors
+        .nFAULT_pin = nFAULT_Pin,
+    }
+},{
+    //M1
+    .axis_config = {
+#if HW_VERSION_MAJOR == 3 && HW_VERSION_MINOR >= 5
+        .step_port = GPIO_7_GPIO_Port,
+        .step_pin = GPIO_7_Pin,
+        .dir_port = GPIO_8_GPIO_Port,
+        .dir_pin = GPIO_8_Pin,
+        //.motor_enable_pin = GPIO_5_Pin,
+        //.motor_enable_port = GPIO_5_GPIO_Port,
+#else
+        .step_port = GPIO_3_GPIO_Port,
+        .step_pin = GPIO_3_Pin,
+        .dir_port = GPIO_4_GPIO_Port,
+        .dir_pin = GPIO_4_Pin,
+        //.motor_enable_pin = GPIO_5_Pin,
+        //.motor_enable_port = GPIO_5_GPIO_Port,
+#endif
+#if HW_VERSION_MAJOR == 3 && HW_VERSION_MINOR >= 3
+        .thermistor_adc_ch = 4,
+#else
+        .thermistor_adc_ch = 1,
+#endif
+        .thread_priority = osPriorityHigh,
+    },
+    .encoder_config = {
+        .timer = &htim4,
+        .index_port = M1_ENC_Z_GPIO_Port,
+        .index_pin = M1_ENC_Z_Pin,
+        .hallA_port = M1_ENC_A_GPIO_Port,
+        .hallA_pin = M1_ENC_A_Pin,
+        .hallB_port = M1_ENC_B_GPIO_Port,
+        .hallB_pin = M1_ENC_B_Pin,
+        .hallC_port = M1_ENC_Z_GPIO_Port,
+        .hallC_pin = M1_ENC_Z_Pin,
+    },
+    .motor_config = {
+        .timer = &htim8,
+        .control_deadline = (3 * TIM_1_8_PERIOD_CLOCKS) / 2,
+        .shunt_conductance = 1.0f / SHUNT_RESISTANCE,  //[S]
+    },
+    .gate_driver_config = {
+        .spi = &hspi3,
+        // Note: this board has the EN_Gate pin shared!
+        .enable_port = EN_GATE_GPIO_Port,
+        .enable_pin = EN_GATE_Pin,
+        .nCS_port = M1_nCS_GPIO_Port,
+        .nCS_pin = M1_nCS_Pin,
+        .nFAULT_port = nFAULT_GPIO_Port, // the nFAULT pin is shared between both motors
+        .nFAULT_pin = nFAULT_Pin,
+    }
+} };
+#endif
+
+
+
+#define I2C_A0_PORT GPIO_3_GPIO_Port
+#define I2C_A0_PIN GPIO_3_Pin
+#define I2C_A1_PORT GPIO_4_GPIO_Port
+#define I2C_A1_PIN GPIO_4_Pin
+#define I2C_A2_PORT GPIO_5_GPIO_Port
+#define I2C_A2_PIN GPIO_5_Pin
+
+#endif // __BOARD_CONFIG_H
