c48251f3f598a0cd39f40740fc7974d82719d201
Unknown <pfguenette@gmail.com>
2018-09-30 02:04:37 -0400

    Report axis errors and current state in heartbeat

--- Firmware/communication/interface_can.cpp
+++ Firmware/communication/interface_can.cpp
@@ -41,6 +41,8 @@
 #include <cmsis_os.h>
 #include <stm32f4xx_hal.h>
 
+#include <odrive_main.h>
+
 std::unordered_map<CAN_HandleTypeDef *, ODriveCAN *> ctxMap;
 
 // Constructor is called by communication.cpp and the handle is assigned appropriately
@@ -65,6 +67,10 @@ void ODriveCAN::can_server_thread() {
         }
 
         // Handle heartbeat message
+        heartbeat.buf[0] = axes[0]->error_;
+        heartbeat.buf[1] = axes[0]->current_state_;
+        heartbeat.buf[2] = axes[1]->error_;
+        heartbeat.buf[3] = axes[2]->current_state_;
         uint32_t now = osKernelSysTick();
         if(now - lastHeartbeatTick >= 100){
             write(heartbeat);
