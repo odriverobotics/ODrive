b09d1e9e256e31059c6a9faf6467586fe5e5cbdc
Oskar Weigl <oskar.weigl@gmail.com>
2018-10-22 21:03:13 -0700

    make step/dir pins configurable

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,6 +1,9 @@
 # Unreleased Features
 Please add a note of your changes below this heading if you make a Pull Request.
 
+## Added
+* Make step dir gpio pins configurable.
+
 ## Fixed
 * Would ERROR_CONTROL_DEADLINE_MISSED along with every ERROR_PHASE_RESISTANCE_OUT_OF_RANGE.
 
--- Firmware/MotorControl/main.cpp
+++ Firmware/MotorControl/main.cpp
@@ -65,6 +65,8 @@ void load_configuration(void) {
             motor_configs[i] = Motor::Config_t();
             trap_configs[i] = TrapezoidalTrajectory::Config_t();
             axis_configs[i] = Axis::Config_t();
+            // Default step/dir pins are different, so we need to explicitly load them
+            Axis::load_default_step_dir_pin_config(hw_configs[i].axis_config, &axis_configs[i]);
         }
     } else {
         user_config_loaded_ = true;
@@ -179,7 +181,7 @@ int odrive_main(void) {
 #if HW_VERSION_MAJOR == 3 && HW_VERSION_MINOR >= 3
     if (board_config.enable_uart) {
         axes[0]->config_.enable_step_dir = false;
-        axes[0]->set_step_dir_enabled(false);
+        axes[0]->set_step_dir_active(false);
         SetGPIO12toUART();
     }
 #endif
