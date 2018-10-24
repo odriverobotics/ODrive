8816a42aacfae92348e675e49422070ebe3b3c31
Oskar Weigl <oskar.weigl@gmail.com>
2018-10-23 23:45:30 -0700

    bake patch: disable IRQ for DMA2_Stream0

--- Firmware/Board/v3/Src/dma.c
+++ Firmware/Board/v3/Src/dma.c
@@ -84,8 +84,10 @@ void MX_DMA_Init(void)
   HAL_NVIC_SetPriority(DMA1_Stream5_IRQn, 5, 0);
   HAL_NVIC_EnableIRQ(DMA1_Stream5_IRQn);
   /* DMA2_Stream0_IRQn interrupt configuration */
-  HAL_NVIC_SetPriority(DMA2_Stream0_IRQn, 5, 0);
-  HAL_NVIC_EnableIRQ(DMA2_Stream0_IRQn);
+  // Dear STM, no we _don't_ want to fire an interrupt for this DMA
+  // (it's not possible to deselect this in CubeMX)
+  //HAL_NVIC_SetPriority(DMA2_Stream0_IRQn, 5, 0);
+  //HAL_NVIC_EnableIRQ(DMA2_Stream0_IRQn);
 
 }
 
