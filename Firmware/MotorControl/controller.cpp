301d68adc6d6f6de2c4669e7bf025cbc56c25a19
Paul Guenette <pfguenette@gmail.com>
2019-05-23 18:56:34 +0200

    Move anticogging_t into config, use a fixed size array and scale

--- Firmware/MotorControl/controller.cpp
+++ Firmware/MotorControl/controller.cpp
@@ -1,6 +1,7 @@
 
 #include "odrive_main.h"
 
+#include <algorithm>
 
 Controller::Controller(Config_t& config) :
     config_(config)
@@ -69,8 +70,8 @@ void Controller::move_incremental(float displacement, bool from_goal_point = tru
 
 void Controller::start_anticogging_calibration() {
     // Ensure the cogging map was correctly allocated earlier and that the motor is capable of calibrating
-    if (anticogging_.cogging_map != NULL && axis_->error_ == Axis::ERROR_NONE) {
-        anticogging_.calib_anticogging = true;
+    if (axis_->error_ == Axis::ERROR_NONE) {
+        config_.anticogging.calib_anticogging = true;
     }
 }
 
@@ -82,20 +83,20 @@ void Controller::start_anticogging_calibration() {
  * This holding current is added as a feedforward term in the control loop.
  */
 bool Controller::anticogging_calibration(float pos_estimate, float vel_estimate) {
-    if (anticogging_.calib_anticogging && anticogging_.cogging_map != NULL) {
-        float pos_err = anticogging_.index - pos_estimate;
-        if (fabsf(pos_err) <= anticogging_.calib_pos_threshold &&
-            fabsf(vel_estimate) < anticogging_.calib_vel_threshold) {
-            anticogging_.cogging_map[anticogging_.index++] = vel_integrator_current_;
+    if (config_.anticogging.calib_anticogging) {
+        float pos_err = config_.anticogging.index - pos_estimate;
+        if (fabsf(pos_err) <= config_.anticogging.calib_pos_threshold &&
+            fabsf(vel_estimate) < config_.anticogging.calib_vel_threshold) {
+            config_.anticogging.cogging_map[std::clamp(config_.anticogging.index++, 0, 3600)] = vel_integrator_current_;
         }
-        if (anticogging_.index < axis_->encoder_.config_.cpr) { // TODO: remove the dependency on encoder CPR
-            set_pos_setpoint(anticogging_.index, 0.0f, 0.0f);
+        if (config_.anticogging.index < 3600) {
+            set_pos_setpoint(config_.anticogging.index * config_.anticogging.cogging_ratio, 0.0f, 0.0f);
             return false;
         } else {
-            anticogging_.index = 0;
+            config_.anticogging.index = 0;
             set_pos_setpoint(0.0f, 0.0f, 0.0f);  // Send the motor home
-            anticogging_.use_anticogging = true;  // We're good to go, enable anti-cogging
-            anticogging_.calib_anticogging = false;
+            config_.anticogging.use_anticogging = true;  // We're good to go, enable anti-cogging
+            config_.anticogging.calib_anticogging = false;
             return true;
         }
     }
@@ -103,9 +104,9 @@ bool Controller::anticogging_calibration(float pos_estimate, float vel_estimate)
 }
 
 bool Controller::update(float pos_estimate, float vel_estimate, float* current_setpoint_output) {
-    // Only runs if anticogging_.calib_anticogging is true; non-blocking
+    // Only runs if config_.anticogging.calib_anticogging is true; non-blocking
     anticogging_calibration(pos_estimate, vel_estimate);
-    float anticogging_pos = pos_estimate;
+    float anticogging_pos = pos_estimate / config_.anticogging.cogging_ratio;
 
     // Trajectory control
     if (config_.control_mode == CTRL_MODE_TRAJECTORY_CONTROL) {
@@ -179,8 +180,8 @@ bool Controller::update(float pos_estimate, float vel_estimate, float* current_s
     // Anti-cogging is enabled after calibration
     // We get the current position and apply a current feed-forward
     // ensuring that we handle negative encoder positions properly (-1 == motor->encoder.encoder_cpr - 1)
-    if (anticogging_.use_anticogging) {
-        Iq += anticogging_.cogging_map[mod(static_cast<int>(anticogging_pos), axis_->encoder_.config_.cpr)];
+    if (config_.anticogging.use_anticogging) {
+        Iq += config_.anticogging.cogging_map[std::clamp(mod(static_cast<int>(anticogging_pos), axis_->encoder_.config_.cpr), 0, 3600)];
     }
 
     float v_err = vel_des - vel_estimate;
