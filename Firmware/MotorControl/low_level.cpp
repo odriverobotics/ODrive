47bcdfa08f60b2ca49c7228722319cd9bf3f558f
Oskar Weigl <oskar.weigl@gmail.com>
2019-09-09 20:42:10 -0700

    fix broken analog feature due to out of RAM for stack

--- Firmware/MotorControl/low_level.cpp
+++ Firmware/MotorControl/low_level.cpp
@@ -753,6 +753,6 @@ static void analog_polling_thread(void *)
 
 void start_analog_thread()
 {
-    osThreadDef(thread_def, analog_polling_thread, osPriorityLow, 0, 4*512);
+    osThreadDef(thread_def, analog_polling_thread, osPriorityLow, 0, 128);
     osThreadCreate(osThread(thread_def), NULL);
 }
