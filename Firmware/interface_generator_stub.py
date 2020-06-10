8c6ab4bd3084af8a77b49343939f11ebc5797071
Samuel Sadok <samuel.sadok@bluewin.ch>
2020-06-10 15:36:47 +0200

    add compile prerequisites to CI

--- Firmware/interface_generator_stub.py
+++ Firmware/interface_generator_stub.py
@@ -5,7 +5,7 @@ import os
 
 try:
     exec(open(os.path.join(os.path.dirname(os.path.realpath(__file__)), 'fibre', 'tools', 'interface_generator.py')).read())
-except ModuleNotFoundError as ex:
+except ImportError as ex:
     print(str(ex), file=sys.stderr)
     print("Note that there are new compile-time dependencies since around v0.5.1.", file=sys.stderr)
     print("Check out https://github.com/madcowswe/ODrive/blob/devel/docs/developer-guide.md#prerequisites for details.", file=sys.stderr)
