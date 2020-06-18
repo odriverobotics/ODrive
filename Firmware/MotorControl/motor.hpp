f19f57779bdcb2267a3ec1925c9d70230ee92b6e
PAJohnson <pajohns4@ncsu.edu>
2020-06-18 18:27:19 -0400

    Changed vel_gain, vel_integrator_gain and torque_constant to work out of the box with ODrive branded motors. Removed unused variable torque_lim_margin Changed default value of torque_lim to +inf

--- Firmware/MotorControl/motor.hpp
+++ Firmware/MotorControl/motor.hpp
@@ -45,15 +45,14 @@ public:
         float resistance_calib_max_voltage = 2.0f; // [V] - You may need to increase this if this voltage isn't sufficient to drive calibration_current through the motor.
         float phase_inductance = 0.0f;        // to be set by measure_phase_inductance
         float phase_resistance = 0.0f;        // to be set by measure_phase_resistance
-        float torque_constant = 1.0f;         // [Nm/A] for PM motors, [Nm/A^2] for induction motors. To be set by user
+        float torque_constant = 0.04f;         // [Nm/A] for PM motors, [Nm/A^2] for induction motors. Equal to 8.27/Kv of the motor
         int32_t direction = 0;                // 1 or -1 (0 = unspecified)
         MotorType motor_type = MOTOR_TYPE_HIGH_CURRENT;
         // Read out max_allowed_current to see max supported value for current_lim.
         // float current_lim = 70.0f; //[A]
-        float current_lim = 10.0f;  //[A]
-        float current_lim_margin = 8.0f;  // Maximum violation of current_lim
-        float torque_lim = 10.0f;           //[Nm]
-        float torque_lim_margin = 8.0f; 
+        float current_lim = 10.0f;          //[A]
+        float current_lim_margin = 8.0f;    // Maximum violation of current_lim
+        float torque_lim = std::numeric_limits<float>::infinity();           //[Nm]. 
         // Value used to compute shunt amplifier gains
         float requested_current_range = 60.0f; // [A]
         float current_control_bandwidth = 1000.0f;  // [rad/s]
