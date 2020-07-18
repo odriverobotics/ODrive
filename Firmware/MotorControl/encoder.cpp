00845fcc65e040de1a360b7d2b0f09dcd309118b
NhanPA <annhandt09@gmail.com>
2020-07-18 07:32:38 +0700

    Update encoder.cpp

--- Firmware/MotorControl/encoder.cpp
+++ Firmware/MotorControl/encoder.cpp
@@ -475,15 +475,13 @@ bool Encoder::update() {
             if (abs_spi_pos_updated_ == false) {
                 // Low pass filter the error
                 spi_error_rate_ += current_meas_period * (1.0f - spi_error_rate_);
-                if (spi_error_rate_ > 0.005f)
-                    set_error(ERROR_ABS_SPI_COM_FAIL);
+                if (spi_error_rate_ > 0.005f) set_error(ERROR_ABS_SPI_COM_FAIL);
             } else {
                 // Low pass filter the error
                 spi_error_rate_ += current_meas_period * (0.0f - spi_error_rate_);
                 if ((spi_error_rate_ < 0.00005f) & (error_==ERROR_ABS_SPI_COM_FAIL)) {
                     error_=ERROR_NONE;
-                    //Axis::ERROR_ENCODER_FAILED= 0x100 000100000000 ->xóa and 111011111111 EFF
-                    axis_->error_ |= Axis::ERROR_ENCODER_OK;
+                    axis_->error_ |= Axis::ERROR_ENCODER_OK;//Axis::ERROR_ENCODER_FAILED= 0x100 000100000000 ->xóa and 111011111111 EFF
                     }
             }
 
