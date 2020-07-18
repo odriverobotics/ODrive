0542233d33c196042a63b17321126b48853f3335
NhanPA <annhandt09@gmail.com>
2020-07-18 07:57:54 +0700

    fix: change or to and

--- Firmware/MotorControl/encoder.cpp
+++ Firmware/MotorControl/encoder.cpp
@@ -481,7 +481,7 @@ bool Encoder::update() {
                 spi_error_rate_ += current_meas_period * (0.0f - spi_error_rate_);
                 if ((spi_error_rate_ < 0.00005f) & (error_==ERROR_ABS_SPI_COM_FAIL)) {
                     error_=ERROR_NONE;
-                    axis_->error_ |= Axis::ERROR_ENCODER_OK;//Axis::ERROR_ENCODER_FAILED= 0x100 000100000000 ->xóa and 111011111111 EFF
+                    axis_->error_ &= Axis::ERROR_ENCODER_OK;//Axis::ERROR_ENCODER_FAILED= 0x100 000100000000 ->xóa and 111011111111 EFF
                     }
             }
 
