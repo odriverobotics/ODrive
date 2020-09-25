03214c8dbe5fb90ff225194d7b468a754f275069
PAJohnson <pajohns4@ncsu.edu>
2020-09-25 00:27:42 -0400

    Added application icon

--- GUI/src/background.js
+++ GUI/src/background.js
@@ -68,7 +68,8 @@ function createWindow() {
       // Use pluginOptions.nodeIntegration, leave this alone
       // See nklayman.github.io/vue-cli-plugin-electron-builder/guide/security.html#node-integration for more info
       nodeIntegration: process.env.ELECTRON_NODE_INTEGRATION
-    }
+    },
+    icon: path.join(__static, 'icon.png')
   })
   win.maximize();
   win.setMenu(null);
