c9de09e12d1b86dfaf22aa71ac85fca6f8cb9c42
Samuel Sadok <samuel.sadok@bluewin.ch>
2021-02-04 22:27:56 +0100

    expand documentation on CanBusBase

--- Firmware/interfaces/canbus.hpp
+++ Firmware/interfaces/canbus.hpp
@@ -28,9 +28,10 @@ public:
      * For interfaces that don't support CAN FD, the function returns false if
      * the two baud rates mismatch.
      * 
-     * @param nominal_baud_rate: The CAN message header baud rate in bits per
-     *        second.
-     * @param data_baud_rate: The payload baud rate in bits per second.
+     * @param nominal_baud_rate: The baud rate that is used for the arbitration
+     *        phase, or, if bit rate switching is not used, the whole message.
+     * @param data_baud_rate: The baud rate that is used for the payload of a
+     *        CAN FD message that uses bit rate switching.
      */
     virtual bool is_valid_baud_rate(uint32_t nominal_baud_rate, uint32_t data_baud_rate) = 0;
 
@@ -40,14 +41,16 @@ public:
      * When the CAN bus is up (and only then), send_message() can be called and
      * the subscriptions get notified on corresponding incoming messages.
      * 
-     * @param nominal_baud_rate: The CAN message header baud rate in bits per
-     *        second.
-     * @param data_baud_rate: The payload baud rate in bits per second.
+     * @param nominal_baud_rate: The baud rate that is used for the arbitration
+     *        phase, or, if bit rate switching is not used, the whole message.
+     * @param data_baud_rate: The baud rate that is used for the payload of
+     *        CAN FD messages that use bit rate switching.
      * @param rx_event_loop: This callback is used to put event tasks on the
      *        caller's event loop. The callback can be called in interrupt
      *        context. See also `subscribe()`.
      * @param on_error: called when an error condition occurs. A bool argument
-     *        is passed to indicate if the error is permanent and the CAN bus is down.
+     *        is passed to indicate if the error is permanent and the CAN bus is
+     *        down.
      * 
      * @returns: True if the CAN bus was started, false otherwise. A possible
      *           reason for a failed start is an incompatible baud rate.
@@ -62,6 +65,13 @@ public:
     /**
      * @brief Sends the specified CAN message.
      * 
+     * @param tx_slot: The TX slot into which to place this message. If an
+     *        earlier message is still pending in this slot it will be evicted
+     *        by the new message.
+     *        The number of available TX slots is implementation specific.
+     * @param message: The message to send.
+     * @param on_sent: A callback that is invoked when the message was
+     *        successfully sent. Can be invoked in an interrupt context.
      * @returns: true on success or false otherwise (e.g. if the send queue is
      * full).
      */
@@ -73,8 +83,16 @@ public:
      * 
      * This function can be used regardless of started/stopped state of the CAN
      * bus.
+     * If several overlapping filters are registered, one or more of them may
+     * be called, depending on the implementation.
      * 
-     * @param on_received: Called when a matching message arrives. This is executed
+     * @param rx_slot: Identifies the RX FIFO or buffer into which messages
+     *        matching this filter should be placed.
+     *        The number of available RX slots is implementation specific and
+     *        can be as low as one.
+     * @param filter: Specifies which messages to accept on this subscription.
+     * @param on_received: Called when a matching message arrives. This is
+     *        executed in the execution context given by `rx_event_loop`.
      * @param handle: On success this handle is set to an opaque pointer that
      *        can be used to cancel the subscription.
      * 
